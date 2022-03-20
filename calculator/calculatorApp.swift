f5b1fecfb2a6db7a1ec70eb31ef2f23cea06bd01
Saurav Sagar <sagarsaurav1997@gmail.com>
2022-03-20 12:09:03 +0530

    init project setup

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Saurav Sagar on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
